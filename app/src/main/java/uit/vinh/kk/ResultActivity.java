1e32c9a5858006c6291b0f0918b046d17d128e8c
VINH <15521020@gm.uit.edu.vn>
2019-12-19 01:47:07 +0700

    add classification result toolbar

---
+++ app/src/main/java/uit/vinh/kk/ResultActivity.java
@@ -0,0 +1,45 @@
+package uit.vinh.kk;
+
+import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class ResultActivity extends AppCompatActivity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState){
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_classificationresult);
+//        Toolbar toolbar = findViewById(R.id.displaypatientinfo);
+//
+//        setSupportActionBar(toolbar);
+//        getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+//        getSupportActionBar().setDisplayShowHomeEnabled(true);
+        getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+
+//        Drawable drawable= getResources().getDrawable(android.R.drawable.ic_menu_search);
+//        getSupportActionBar().setHomeAsUpIndicator(drawable);
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.menu_back_menuitem, menu);
+        //getMenuInflater().inflate(R.menu.menu_search, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        switch (item.getItemId())
+        {
+            case android.R.id.home:
+                onBackPressed();
+                return true;
+
+            default:break;
+        }
+
+        return super.onOptionsItemSelected(item);
+    }
+}
